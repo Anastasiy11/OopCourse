77dd225961be3ca998f2fd2b8197c614c9f8c457
anast <anastasin980227@.gmail.com>
2024-07-11 13:58:33 +0700

    Add Range project

---
+++ Range/src/ru/academits/bryanskaya/range.main/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.bryanskaya.range.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Range/src/ru/academits/bryanskaya/range/Range.java
@@ -0,0 +1,4 @@
+package ru.academits.bryanskaya.range;
+
+public class Range {
+}
