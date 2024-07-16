619fd43978285c8315e7e976ce03e6094b0da041
anast <anastasin980227@.gmail.com>
2024-07-16 13:46:48 +0700

    Add Range project

--- Range/src/ru/academits/bryanskaya/range.main/Main.java
+++ Range/src/ru/academits/bryanskaya/range.main/Main.java
@@ -1,7 +1,38 @@
 package ru.academits.bryanskaya.range.main;
 
+import ru.academits.bryanskaya.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите начальное число диапазона:");
+        double from = scanner.nextDouble();
+
+        System.out.println("Введите конечное число диапазона:");
+        double to = scanner.nextDouble();
+
+        System.out.println("Введите число для проверки:");
+        double numberToCheck = scanner.nextDouble();
+
+        Range range = new Range(from, to);
+
+        if(range.isInside(numberToCheck)){
+            System.out.println("Число в пределах диапазона.");
+        }else {
+            System.out.println("Число за пределами диапазона.");
+        }
+
+        System.out.println("Длина диапазона от " + from + " до " + to + " = " + range.getLength(from, to));
+
+        range.setFrom(4);
+        range.setTo(12);
+
+        double newFrom = range.getFrom();
+        double newTo = range.getTo();
+
+        System.out.println("Длина диапазона от " + newFrom + " до " + newTo + " = " + range.getLength(newFrom, newTo));
     }
 }
